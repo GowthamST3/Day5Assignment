91eb2cbeb6b5a36614a2eb9b4eff75ec26209bd1
GowthamST3 <gowthamgiri3@gmail.com>
2022-04-18 22:21:27 +0530

    LeapYear Code

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+package com.BridgeLabz;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+	 	System.out.println("Enter Any Year...");
+		int year = s.nextInt();
+
+		if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+			System.out.println(year + " is Leap Year.... ");
+		else
+			System.out.println(year + " is Not Leap  Year.... ");
+
+	}
+
+}
